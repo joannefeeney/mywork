c8dd4386d3270a0ec4b6ec083a4a886643b7f97c
Joanne <G00283773@atu.ie>
2023-02-12 20:14:13 +0000

    week02 adding task

---
+++ week02/addOne.py
@@ -0,0 +1,7 @@
+# hello.py
+# Author: Joanne Feeney
+# This program reads a number and prints out one more than that number
+
+number = int(input('please enter a number:'))
+newNumber = number + 1
+print(f'{number} plus one is {newNumber}')
