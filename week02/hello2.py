f9a3663657a96b77d067ebfea6e2595c74516d3a
Joanne <G00283773@atu.ie>
2023-02-12 20:10:09 +0000

    week02 task

---
+++ week02/hello2.py
@@ -0,0 +1,6 @@
+# hello.py
+# Author: Joanne Feeney
+# This program reads in a person’s name and prints out hello and that persons name
+
+name = input("Enter your name:")
+print('Hello ' + name + '\nNice to meet you')
