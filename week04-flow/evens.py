2ed020b566d399995a95ea796f9f29028b5c3b5b
Joanne <G00283773@atu.ie>
2023-02-18 14:27:03 +0000

    This program prints all number from 2 to 100

---
+++ week04-flow/evens.py
@@ -0,0 +1,10 @@
+# evens.py
+# Author: Joanne Feeney
+# This program uses a while loop to print all the even numbers from 2 to 100
+
+numberTo = 100
+evenNum = 2
+while evenNum <= numberTo:
+    print (evenNum)
+    evenNum += 2
+
