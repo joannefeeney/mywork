f3c2ad43920e4e543187e16529f0dd71efd89ae2
Joanne <G00283773@atu.ie>
2023-03-13 22:08:42 +0000

    week 05 lab

---
+++ week05/week05-datastructures.py
@@ -0,0 +1,51 @@
+# week05-datastructures.py
+# Author: Joanne Feeney
+# This program is a copy of the questions for week 05's lab
+
+numberOfQuestions = 5
+averageAge = 23.4
+debugMode = True
+name = "joe"
+ages = []
+months = ('Jan', 'Feb', 'Mar')
+book = {}
+stuff = [ 12 , 'Fred', False, {}]
+someone = dict(firstname = "joe")
+me = {
+"firstName" : "Andrew",
+"teaching" : [{
+"courseName" : "programming",
+"semester" : 1
+},{
+"courseName" : "Data Representation",
+"semester" : 2
+}
+]
+
+
+''' 
+What are the variable types of the following variables in the code above 
+a. numberOfQuestions
+b. averageAge
+c. debugMode
+d. name
+e. ages
+f. months
+g. months[1]
+h. book
+i. stuff
+j. stuff[2]
+k. someone
+l. someone["firstname"]
+m. me
+n. me["teaching"]
+o. me["teaching"][0]["semester"]
+p is a trick question look at it carefully
+p. me["teaching"][0]["coursename"]
+'''
+
+# Answer = 
+
+
+
+
