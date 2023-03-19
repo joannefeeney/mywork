833239c2ab6f3695359580cc65bf91451a8c88f2
Joanne <G00283773@atu.ie>
2023-03-19 20:45:05 +0000

    week 06 lab

---
+++ week06/week06_JF_messing.py
@@ -0,0 +1,27 @@
+# week 06 lab
+# Author: Joanne Feeney
+# This program is used to answer week 06's lab questions
+
+# 1.
+
+# function yo takes one parameter
+def yo(a):
+ return a * 2
+# here we are calling the function yo
+# and are passing in an argument, 3
+yo(3)
+
+'''
+Look at the program above:
+a. What will be printed out when this program is run?
+b. What would you have to add to the program to get it to print out what
+the calling the function yo(3) returns?
+'''
+
+'''
+a. I think 6 will be printed when this program is run
+b. Do not understand the question but if it is asked what needs to be added to the program 
+  for an answer to be seen then: print(f"(yo(3))")
+'''
+
+print(yo(3))
